2a33038b387495b2482f7541abf233e5793f278e
Syeda Sabahat <159911882+sabahat97@users.noreply.github.com>
2024-03-30 02:16:15 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,32 @@
+//question 49 
+//tell your hobbies
+function myhobbies(a : string, b : string, c : string){
+   return a + b + c 
+}
+
+let hobbies: string= myhobbies("TRAVELLING,","EATING,","& Nothing");
+
+console.log (`MY HOBBIES ARE ${hobbies}`);
+
+
+//ex 50
+//using multiline string
+let myIdealDay : string = `My ideal days are used to ,
+
+1.Wakeup
+2.Fresh
+3.Breakfast
+4.Coding
+5.Eating
+6.Sleeping
+and repeat 0n.`
+
+console.log (myIdealDay);
+
+ //ex51
+ //calculate area of retrangle
+ function calcArea(width : number, height : number){
+ return width * height
+ }
+ let calculate = calcArea(10,8);
+ console.log (calculate);
